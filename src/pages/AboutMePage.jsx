8c2066a4d9b44045bdb853a70c6480a5d97b1a58
phoebe neo <phoebe.neo.2023@scis.smu.edu.sg>
2024-11-26 03:58:12 +0800

    pictures

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -3,20 +3,43 @@ import React from 'react';
 const AboutMePage = () => (
   <div style={{ textAlign: 'center', padding: '20px' }}>
     <h1>About Me</h1>
+
+    {/* myself */}
     <div>
-    <img
-      src="/assets/images/cat.jpg"
-      alt="Phoebe"
-      className="image"
-    />
+      <img
+        src="/assets/images/cat.jpg"
+        alt="Phoebe"
+        className="image"
+      />
     </div>
     <p style={{ fontSize: '18px', color: '#333', lineHeight: '1.6' }}>
-      Hello~ I'm <strong>Phoebe</strong>, the creator of this website and a student at 
-      <strong> Singapore Management University</strong>. I am passionate about Data Science, Software Engineering, 
+      Hello~ I'm <strong>Phoebe</strong>, the creator of this website and a student at
+      <strong> Singapore Management University</strong>. I am passionate about Data Science, Software Engineering,
       Cute Animals (like cats and polar bears!), and Environmental Sustainability.
     </p>
+
+    {/* inuka */}
     <p style={{ fontSize: '18px', color: '#333', lineHeight: '1.6' }}>
-      This site is a personal project I created to learn more about <strong>React</strong>, <strong>Three.js</strong>, and 
+      I'm from Singapore, the same country as <strong>Inuka</strong>, the first polar bear to be born in the Tropics and the last Polar Bear to even be seen in Singapore after Wildlife Reserves Singapore (WRS) announced that it will not bring any more polar bears to Singapore. The pictures below shows Baby Inuka and his mother Sheba.
+    </p>
+    <div>
+      <img
+        src="/assets/images/inuka and sheba.jpg"
+        alt="Phoebe"
+        className="image"
+      />
+    </div>
+    <div>
+      <img
+        src="/assets/images/inuka.jpg"
+        alt="Phoebe"
+        className="image"
+      />
+    </div>
+
+    {/* project */}
+    <p style={{ fontSize: '18px', color: '#333', lineHeight: '1.6' }}>
+      This site is a personal project I created to learn more about <strong>React</strong>, <strong>Three.js</strong>, and
       <strong> Vercel</strong> while aiming to inspire people to take action for polar bears and our environment. Additionally, I got to explore <strong>Time Series Forecasting</strong> and <strong> Predictive Modeling</strong> methods by analyzing and forecasting polar bear population dynamics using <strong>Sea Ice Data</strong>. The visualizations and insights are shared in the
       <a
         href="/Population.jsx"
@@ -31,7 +54,7 @@ const AboutMePage = () => (
       >
         <strong>Population</strong>
       </a>
-page, and the dataset used is publicly available at: 
+      page, and the dataset used is publicly available at:
       <a
         href="https://datadryad.org/stash/dataset/doi:10.5061/dryad.f68m0"
         target="_blank"
@@ -47,16 +70,23 @@ page, and the dataset used is publicly available at:
       </a>
     </p>
     <p style={{ fontSize: '18px', color: '#333', lineHeight: '1.6' }}>
-      I hope you enjoy exploring the site and learning more about these amazing creatures as well as the challenges 
+      I hope you enjoy exploring the site and learning more about these amazing creatures as well as the challenges
       they face due to climate change!
     </p>
     <div>
-    <img
-      src="/assets/images/phoebepolar.jpg"
-      alt="Phoebe"
-      className="image"
-    />
+      <img
+        src="/assets/images/phoebepolar.jpg"
+        alt="Phoebe"
+        className="image"
+      />
     </div>
+    {/* <div>
+      <img
+        src="/assets/images/polar bear eats.jpg"
+        alt="Phoebe"
+        className="image"
+      />
+    </div> */}
   </div>
 );
 
